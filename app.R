ba9c5b0b24440e288527bd22fb33f16a5f43d203
EEgge <elianne.egge@gmail.com>
2021-02-03 20:28:21 +0100

    Commit 03.02.2021

--- app.R
+++ app.R
@@ -8,143 +8,53 @@
 #
 
 library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(purrr)
+library(vegan)
+library(here)
+library(plotly)
+library(ggpubr)
+library(data.table)
+library(vegan)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Interactive figures from MicroPolar protist metabarcoding data paper"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            numericInput("prop_lim", "Taxonomic groups present with < x% in all samples will be grouped together as 'Other'",
-                         value = 5, min = 0, max = 100),
-            radioButtons("which_tab", "ASV-table with separate or merged replicates:", 
-                         choices = c("Merged" = "merged", "Separate" = "separate"), selected = "merged"),
-            checkboxGroupInput("taxo_group2", label = "Division", 
-                               choices = c("Ochrophyta_Bacillariophyta", "Dinoflagellata_Syndiniales", 
-                                           "Dinoflagellata_Dinophyceae", "Haptophyta", "Ciliophora", "Chlorophyta", "Picozoa", "Stramenopiles_X", "Pseudofungi",  "Radiolaria",  "All"), 
-                               selected = "All"),
-            radioButtons("taxlevel_barplot", label = "Taxonomic level",
-                         choices = c("Division" = "Divisionlong", "Class"="Class", "Order"="Order", "Family"="Family", "Genus"="Genus", "Species"="Species")),
-            radioButtons("propchoice", label = "Proportion of selected group or all", choices = c("Selected group" = "selectgroup","All" = "All"))
-            
-        ),
+# asvtab_wtax <- read_excel(here("metapr2/export", "metapr2_wide_asv_set_207_208_209_Eukaryota_nodups.xlsx"))
+# asvtab_wtax <- asvtab_wtax %>% add_column(divisionlong = NA)
+# for (i in 1:dim(asvtab_wtax)[1]) {
+#     if (asvtab_wtax$division[i] %in% c("Dinoflagellata", "Ochrophyta")) {
+#         asvtab_wtax$divisionlong[i] <- paste(asvtab_wtax$division[i], asvtab_wtax$class[i], sep = "_")
+#     } else {
+#         asvtab_wtax$divisionlong[i] <- asvtab_wtax$division[i]
+#     }
+# }
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           textOutput("jaja")
-        )
-    )
+asvtab_merg_subsamp_readnum <- read_delim(here("data", "asv_tab_merged_subsamp.txt"), delim = "\t")
+asvtab_subsamp_prop_tax <- read_delim(here("data", "asvtab_subsamp_prop_tax.txt"), delim = "\t")
+meta_tab_seq_event <- read_delim(here("figures_tables/tables", "table1.txt"), delim = "\t")
+meta_tab_sample_sf <- meta_tab_seq_event[!duplicated(meta_tab$sample_sizefract),]
+
+
+source("panels/panel_ui_barplot_dp.R", local = TRUE)
+source("panels/panel_ui_rarefaction_dp.R", local = TRUE)
+
+ui <- navbarPage(
+    "Interactive figures from MicroPolar protist metabarcoding data paper",
+    tabPanel("Barplot", barpage),
+    tabPanel("Rarefaction curves", rarefpage)
+    
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
     
-    limfun <- reactive({
-        function(x) {
-            ifelse(x>=input$prop_lim/100,1,0)
-        } 
-    })
+    #Barplot panel
+    source("panels/panel_server_barplot_dp.R", local = TRUE)
     
-    mp_plot <- reactive({ 
-        taxlevel <- input$taxlevel_barplot
-        otutab_sfsep <- otutab_sfsep %>% select(-all_of(three200.2)) #Remove 'fake' size fraction 3-200
-        
-        if (input$taxo_group2 != "All") {
-            otutab_tax0 <- otutab_sfsep %>% filter(.data[[input$taxlevel]] %in% .env[[input$taxo_group2]]) #Create new OTU table with only the selected division
-        } else {
-            otutab_tax0 <- otutab_sfsep
-        }
-        
-        otutab_tax_num <- otutab_tax0 %>% select_if(is.numeric)
-        
-        #### If selected, create new proportions of selected groups ####
-        otutab_tax_num <- otutab_sfsep %>% select_if(is.numeric)
-        if (input$propchoice == "selectgroup") {
-          otutab_tax_selectprop <- sweep(otutab_tax_num, 2 , colSums(otutab_tax_num), FUN = "/")
-        } else {
-        otutab_tax_selectprop <- otutab_tax_num
-         }
-        
-        
-        otutab_tax_notnum <- otutab_sfsep %>% select_if(negate(is.numeric))
-        otutab_tax <- cbind.data.frame(otutab_tax_notnum, otutab_tax_selectprop)
-        otutab_tax[is.na(otutab_tax)] <- 0 #NA's because some samples have 0% of a certain class 
-        
-        #  
-        otutab_tax$total <- otutab_tax %>% select_if(is.numeric) %>% rowSums()
-        taxlevtab <- otutab_tax %>% select(get(taxlevel)) %>% droplevels.data.frame()
-        
-        ########
-        
-        taxgroupspre <-  select(otutab_tax, -total) %>%   group_by_(taxlevel) %>% summarise_if(is.numeric, sum)
-        
-        
-        
-        
-        
-        
-        
-        
-        taxgroupspre_mat <- as.matrix(taxgroupspre[,-1, drop = FALSE])
-        taxgroupspre_bin <- apply(taxgroupspre_mat, 2, limfun())
-        
-        if (is.vector(taxgroupspre_bin)) {
-            taxgroupspre_bin2 <- as.data.frame(as.list(taxgroupspre_bin))
-        } else {
-            taxgroupspre_bin2 <- as.data.frame(taxgroupspre_bin)}
-        
-        
-        
-        taxgroupspre_bin_sums <- taxgroupspre_bin2 %>% select_if(is.numeric) %>% mutate(rowsumm = rowSums(.))
-        taxgroupspre_bin_sums$taxgroups <- as.vector(taxgroupspre[[taxlevel]])
-        taxgroupspre_bin_sums_yes <- filter(taxgroupspre_bin_sums, rowsumm >0) %>% select(taxgroups)
-        
-        
-        
-        
-        taxgroups_select <- taxgroupspre %>% filter(get(taxlevel) %in% taxgroupspre_bin_sums_yes$taxgroups)
-        
-        taxgroups_other <- taxgroupspre %>% filter(!get(taxlevel) %in% taxgroupspre_bin_sums_yes$taxgroups)
-        
-        
-        
-        if (dim(taxgroupspre_bin_sums_yes)[1] < dim(taxgroupspre_bin_sums)[1]) {
-            Taxonomic_group <- c(taxgroupspre_bin_sums_yes$taxgroups,"Other")
-            othersum <- colSums(taxgroups_other[,-1])
-        } else {
-            Taxonomic_group <- c(taxgroupspre_bin_sums_yes$taxgroups)
-            othersum <- NULL
-        }
-        #sjekk_prop <- head(taxgroupspre_mat)[1,] ####14.03.2020
-        
-        if (input$propchoice == "selectgroup") {
-        taxgroups_select2 <- rbind(taxgroups_select[,-1],othersum)
-           taxgroups_select2 <- sweep(taxgroups_select2, 2 , colSums(taxgroups_select2), FUN = "/")
-         } else {
-           taxgroups_select2 <- rbind(taxgroups_select[,-1],othersum)
-         }
-        
-        
-        
-        
-        taxgroups_select3 <- cbind.data.frame(Taxonomic_group,taxgroups_select2)
-        
-        taxgroups_select3t <- t(taxgroups_select3)
-        taxgroups_select3t2 <- taxgroups_select3t[-1,]
-        taxgroups_select3t3 <- apply(taxgroups_select3t2, 2, as.numeric)
-        taxgroups_select3tdf0 <- as.data.frame(taxgroups_select3t3)
-        names(taxgroups_select3tdf0) <- Taxonomic_group
-        
-        
-    })
-    
-    output$jaja <- renderText({
-        limfun()(c(.06,.02))
-    })
-    
+    #Rarefaction panel
+    source("panels/panel_server_rarefaction_dp.R", local = TRUE)
     
 }
 
---
+++ panels/panel_server_barplot_dp.R
@@ -0,0 +1,220 @@
+#### Output ####
+output$barplot_12 <- renderPlot({
+  barplots()$p12
+})
+
+output$barplot_345 <- renderPlot({
+  barplots()$p345
+})
+
+output$barplot_net <- renderPlot({
+  barplots()$pnet
+})
+
+
+output$barplotly_12 <- renderPlotly({
+  barplots()$p12ly
+})
+
+output$barplotly_345 <- renderPlotly({
+  barplots()$p345ly
+})
+
+output$barplotly_net <- renderPlotly({
+  barplots()$pnetly
+})
+
+barplots <- eventReactive(input$actionb_barplot, { 
+  
+  if (input$which_tab == "merged") {
+    asvtab_bp_start <- asvtab_subsamp_prop_tax
+  } else {
+    asvtab_bp_start <- read_delim(here("data", "asvtab_nonmerged_prop_wtax.txt"), delim = "\t")
+  }
+  
+  #### Function to identify taxa that were present with < 5% of the reads in all samples ####
+  limfun <- function(x) {
+      ifelse(x>=input$prop_lim/100,1,0)
+    }
+  
+  taxlevel <- input$taxlevel_barplot
+  
+  
+  #### Filter asv table to keep selected Divisions, unless 'All' is selected ####
+  if (input$taxo_group_barp != "All") {
+    asvtab_filter <- asvtab_bp_start %>% filter(divisionlong %in% input$taxo_group_barp)
+  } else {
+    asvtab_filter <- asvtab_bp_start
+  }
+
+  asvtab_filter_num <- asvtab_filter %>% purrr::keep(is.numeric)
+  asvtab_filter_notnum <- asvtab_filter %>% purrr::keep(negate(is.numeric))
+  
+  #print(dim(asvtab_filter_num))
+  #### If selected, create new proportions of selected Divisions ####
+  if (input$propchoice == "selectgroup") {
+    asvtab_filter_prop <- sweep(asvtab_filter_num, 2 , colSums(asvtab_filter_num), FUN = "/")
+  } else {
+    asvtab_filter_prop <- asvtab_filter_num
+  }
+  
+  #### combine asv table with new proportions with taxonomy ####
+  asvtab_filter_wtax <- cbind.data.frame(asvtab_filter_notnum, asvtab_filter_prop)
+  
+  asvtab_filter_wtax[is.na(asvtab_filter_wtax)] <- 0 #NA's because some samples have 0% of a certain class 
+  
+  #### Group by selected taxonomic level ####
+  groupby_taxlevel <-  asvtab_filter_wtax %>% group_by(.data[[taxlevel]]) %>% summarise_if(is.numeric, sum)
+  
+  ## Transform grouped df to matrix, to be able to apply limfun ##
+  taxgroupspre_mat <- as.matrix(groupby_taxlevel[,-1, drop = FALSE])
+  taxgroupspre_bin <- apply(taxgroupspre_mat, 2, limfun)
+  
+  if (is.vector(taxgroupspre_bin)) {
+    taxgroupspre_bin2 <- as.data.frame(as.list(taxgroupspre_bin))
+  } else {
+    taxgroupspre_bin2 <- as.data.frame(taxgroupspre_bin)}
+  
+  #### Identify taxa present as < selected % in all samples (low prop. taxa) ####
+  taxgroupspre_bin_sums <- taxgroupspre_bin2 %>% select_if(is.numeric) %>% mutate(rowsumm = rowSums(.))
+  taxgroupspre_bin_sums$taxgroups <- groupby_taxlevel %>% pull(get(taxlevel))
+  taxgroupspre_bin_sums_yes <- filter(taxgroupspre_bin_sums, rowsumm >0) %>% select(taxgroups)
+  
+  taxgroups_select <- groupby_taxlevel %>% filter(get(taxlevel) %in% taxgroupspre_bin_sums_yes$taxgroups)
+  taxgroups_other <- groupby_taxlevel %>% filter(!get(taxlevel) %in% taxgroupspre_bin_sums_yes$taxgroups)
+  
+  if (dim(taxgroupspre_bin_sums_yes)[1] < dim(taxgroupspre_bin_sums)[1]) {
+    Taxonomic_group <- c(taxgroupspre_bin_sums_yes$taxgroups,"Other")
+    othersum <- colSums(taxgroups_other[,-1])
+  } else {
+    Taxonomic_group <- c(taxgroupspre_bin_sums_yes$taxgroups)
+    othersum <- NULL
+  }
+  
+
+  taxgroups_select2 <- rbind(taxgroups_select[,-1],othersum)
+  taxgroups_select3 <- cbind.data.frame(Taxonomic_group,taxgroups_select2)
+  
+
+  #### Pivot table ####
+  if (input$which_tab == "merged") {
+  taxlevel_pivot <- pivot_longer(taxgroups_select3, cols = !Taxonomic_group, names_to = "sample_sizefract")
+  } else {
+    taxlevel_pivot <- pivot_longer(taxgroups_select3, cols = !Taxonomic_group, names_to = "seq_event")
+  }
+  
+  #### Join pivoted taxa table with metadata ####
+  if (input$which_tab == "merged") {
+    taxlevel_pivot_wmeta <- left_join(taxlevel_pivot, meta_tab_sample_sf, by = "sample_sizefract")
+  } else {
+    taxlevel_pivot_wmeta <- left_join(taxlevel_pivot, meta_tab_seq_event, by = "seq_event")
+  }
+  
+  #### 03.02.21 her er jeg ####
+  if (input$which_tab == "merged") {
+  taxlevel_pivot_wmeta2 <- taxlevel_pivot_wmeta %>% mutate(station_depth = factor(station_depth, levels = unique(meta_tab$station_depth), ordered = T),
+                                                           month = factor(month, levels = c("Jan", "Mar", "May", "Aug", "Nov"), ordered = T), 
+                                                           collection_method = factor(collection_method, levels = c("niskin", "net")), 
+                                                           size_fraction = factor(size_fraction, levels = c("0.4_3", "3_10", "10_50", "50_200", "3_180"), ordered = T))
+  
+  taxlevel_pivot_wmeta2_jan_mar <- taxlevel_pivot_wmeta2 %>% filter(month %in% c("Jan", "Mar"), fraction_max != 200)
+  
+  p12 <- ggplot(taxlevel_pivot_wmeta2_jan_mar, aes(x=reorder(station_depth, desc(station_depth)), y=value, fill = Taxonomic_group, 
+                                                   text = sprintf("Taxon: %s<br>Sample: %s<br>Percent: %s ", Taxonomic_group, station_depth, round(value*100,3))))+
+    labs(title = "Proportional read abundance")+
+    geom_bar(stat = "identity", position = "stack")+
+    #scale_fill_manual(values = bpcolvec)+
+    facet_grid(rows = vars(month), cols = vars(size_fraction), scales = "free_y", space = "free_y")+
+    theme(axis.title.y = element_blank(),
+          axis.title.x = element_blank())+
+    ylim(0,1.01)+
+    coord_flip()
+  p12
+  p12ly <- ggplotly(p12, tooltip = "text")
+  
+  #### Plot for May, Aug, Nov, Niskin samples ####
+  taxlevel_pivot_wmeta2_may_nov <- taxlevel_pivot_wmeta2 %>% filter(month %in% c("May", "Aug", "Nov"), collection_method == "niskin")
+  
+  p345 <- ggplot(taxlevel_pivot_wmeta2_may_nov, aes(x=reorder(station_depth, desc(station_depth)), y=value, fill = Taxonomic_group,
+                                                    text = sprintf("Taxon: %s<br>Sample: %s<br>Percent: %s ", Taxonomic_group, station_depth, round(value*100,3))))+
+    #labs(title = "Proportional read abundance")+
+    geom_bar(stat = "identity", position = "stack")+
+    #scale_fill_manual(values = bpcolvec)+
+    facet_grid(rows = vars(month), cols = vars(size_fraction), scales = "free_y", space = "free_y")+
+    theme(axis.title.y = element_blank(),
+          axis.title.x = element_blank())+
+    ylim(0,1.01)+
+    coord_flip()
+  
+  p345ly <- ggplotly(p345, tooltip = "text")
+  #### Plot net haul ####
+  taxlevel_pivot_wmeta2_net <- taxlevel_pivot_wmeta2 %>% filter(collection_method == "net")
+  
+  pnet <- ggplot(taxlevel_pivot_wmeta2_net, aes(x=reorder(station_depth, desc(station_depth)), y=value, fill = Taxonomic_group,
+                                                text = sprintf("Taxon: %s<br>Sample: %s<br>Percent: %s ", Taxonomic_group, station_depth, round(value*100,3))))+
+    #labs(title = "Proportional read abundance")+
+    geom_bar(stat = "identity", position = "stack")+
+    #scale_fill_manual(values = bpcolvec)+
+    facet_grid(rows = vars(month), cols = vars(size_fraction), scales = "free_y", space = "free_y")+
+    theme(axis.title.y = element_blank(),
+          axis.title.x = element_blank())+
+    ylim(0,1.01)+
+    coord_flip()
+  pnetly <- ggplotly(pnet, tooltip = "text")
+  
+  } else {
+  
+  taxlevel_pivot_wmeta2 <- taxlevel_pivot_wmeta %>% mutate(station_dep_com = factor(station_dep_com, levels = unique(meta_tab$station_dep_com), ordered = T),
+                                                           month = factor(month, levels = c("Jan", "Mar", "May", "Aug", "Nov"), ordered = T), 
+                                                           collection_method = factor(collection_method, levels = c("niskin", "net")),
+                                                           size_fraction = factor(size_fraction, levels = c("0.4_3", "3_10", "10_50", "50_200", "3_180"), ordered = T))
+  taxlevel_pivot_wmeta2_jan_mar <- taxlevel_pivot_wmeta2 %>% filter(month %in% c("Jan", "Mar"), fraction_max != 200)
+  
+  p12 <- ggplot(taxlevel_pivot_wmeta2_jan_mar, aes(x=reorder(station_dep_com, desc(station_dep_com)), y=value, fill = Taxonomic_group, 
+                                                   text = sprintf("Taxon: %s<br>Sample: %s<br>Percent: %s ", Taxonomic_group, station_dep_com, round(value*100,3))))+
+    labs(title = "Proportional read abundance")+
+    geom_bar(stat = "identity", position = "stack")+
+    #scale_fill_manual(values = bpcolvec)+
+    facet_grid(rows = vars(month), cols = vars(size_fraction), scales = "free_y", space = "free_y")+
+    theme(axis.title.y = element_blank(),
+          axis.title.x = element_blank())+
+    ylim(0,1.01)+
+    coord_flip()
+  p12
+  p12ly <- ggplotly(p12, tooltip = "text")
+  
+  #### Plot for May, Aug, Nov, Niskin samples ####
+  taxlevel_pivot_wmeta2_may_nov <- taxlevel_pivot_wmeta2 %>% filter(month %in% c("May", "Aug", "Nov"), collection_method == "niskin")
+  
+  p345 <- ggplot(taxlevel_pivot_wmeta2_may_nov, aes(x=reorder(station_dep_com, desc(station_dep_com)), y=value, fill = Taxonomic_group,
+                                                    text = sprintf("Taxon: %s<br>Sample: %s<br>Percent: %s ", Taxonomic_group, station_dep_com, round(value*100,3))))+
+    #labs(title = "Proportional read abundance")+
+    geom_bar(stat = "identity", position = "stack")+
+    #scale_fill_manual(values = bpcolvec)+
+    facet_grid(rows = vars(month), cols = vars(size_fraction), scales = "free_y", space = "free_y")+
+    theme(axis.title.y = element_blank(),
+          axis.title.x = element_blank())+
+    ylim(0,1.01)+
+    coord_flip()
+  
+  p345ly <- ggplotly(p345, tooltip = "text")
+  #### Plot net haul ####
+  taxlevel_pivot_wmeta2_net <- taxlevel_pivot_wmeta2 %>% filter(collection_method == "net")
+  
+  pnet <- ggplot(taxlevel_pivot_wmeta2_net, aes(x=reorder(station_dep_com, desc(station_dep_com)), y=value, fill = Taxonomic_group,
+                                                text = sprintf("Taxon: %s<br>Sample: %s<br>Percent: %s ", Taxonomic_group, station_dep_com, round(value*100,3))))+
+    #labs(title = "Proportional read abundance")+
+    geom_bar(stat = "identity", position = "stack")+
+    #scale_fill_manual(values = bpcolvec)+
+    facet_grid(rows = vars(month), cols = vars(size_fraction), scales = "free_y", space = "free_y")+
+    theme(axis.title.y = element_blank(),
+          axis.title.x = element_blank())+
+    ylim(0,1.01)+
+    coord_flip()
+  pnetly <- ggplotly(pnet, tooltip = "text")
+  
+  }
+    
+  return(list(p12 = p12, p345 = p345, pnet = pnet, p12ly = p12ly, p345ly = p345ly, pnetly = pnetly))
+})
+  
